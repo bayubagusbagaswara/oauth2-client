592e5545516238a184644f3d8ba44a90c783acc2
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-23 16:53:00 +0700

    Initial commit

--- src/main/java/com/spring/client/SpringSecurityClientApplication.java
+++ src/main/java/com/spring/client/SpringSecurityClientApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.spring.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SpringSecurityClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SpringSecurityClientApplication.class, args);
 	}
 
 }
