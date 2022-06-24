d24f3015b3c36c58cdb00303f3d75e85eaff2b9c
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-24 10:14:28 +0700

    add implementation service PasswordResetTokenServiceImpl

---
+++ src/main/java/com/spring/client/service/impl/PasswordResetTokenServiceImpl.java
@@ -0,0 +1,74 @@
+package com.spring.client.service.impl;
+
+import com.spring.client.entity.PasswordResetToken;
+import com.spring.client.entity.User;
+import com.spring.client.repository.PasswordResetTokenRepository;
+import com.spring.client.repository.UserRepository;
+import com.spring.client.service.PasswordResetTokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Calendar;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class PasswordResetTokenServiceImpl implements PasswordResetTokenService {
+
+    private final PasswordResetTokenRepository passwordResetTokenRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public PasswordResetTokenServiceImpl(PasswordResetTokenRepository passwordResetTokenRepository, UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.passwordResetTokenRepository = passwordResetTokenRepository;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void createPasswordResetTokenForUser(User user, String token) {
+        PasswordResetToken passwordResetToken = new PasswordResetToken(user, token);
+        passwordResetTokenRepository.save(passwordResetToken);
+    }
+
+    @Override
+    public String validatePasswordResetToken(String token) {
+        PasswordResetToken passwordResetToken = passwordResetTokenRepository.findByToken(token);
+
+        if (passwordResetToken == null) {
+            return "Password Reset Token is Invalid";
+        }
+
+        User user = passwordResetToken.getUser();
+        Calendar calendar = Calendar.getInstance();
+
+        if ((passwordResetToken.getExpirationTime().getTime() - calendar.getTime().getTime()) <= 0) {
+            passwordResetTokenRepository.delete(passwordResetToken);
+            return "Password Reset Token is Expired";
+        }
+
+        return "Password Reset Token is Valid";
+    }
+
+    @Override
+    public Optional<User> getUserByPasswordResetToken(String token) {
+        return Optional.ofNullable(passwordResetTokenRepository.findByToken(token).getUser());
+    }
+
+    @Override
+    public void changePassword(User user, String newPassword) {
+        user.setPassword(passwordEncoder.encode(newPassword));
+        userRepository.save(user);
+    }
+
+    @Override
+    public boolean checkIfValidOldPassword(User user, String oldPassword) {
+        // cek apakah sama oldPassword dengan password milik User itu sama
+        // true jika sama
+        // false jika tidak sama
+        return passwordEncoder.matches(oldPassword, user.getPassword());
+    }
+}
