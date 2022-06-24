a6cc28a54b6b1b705e5f468979098a5f8bf34e8d
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-24 10:03:42 +0700

    update UserService

--- src/main/java/com/spring/client/service/UserService.java
+++ src/main/java/com/spring/client/service/UserService.java
@@ -2,15 +2,10 @@ package com.spring.client.service;
 
 import com.spring.client.dto.UserDTO;
 import com.spring.client.entity.User;
-import com.spring.client.entity.VerificationToken;
 
 public interface UserService {
 
     User registerUser(UserDTO userDTO);
-
-    void saveVerificationTokenForUser(String token, User user);
-
-    String validateVerificationToken(String token);
 
     User findUserByEmail(String email);
 }
