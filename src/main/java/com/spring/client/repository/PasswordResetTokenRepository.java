55c71283cdd3e7faf60a0053e7f6905aa1d042dc
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-24 10:11:47 +0700

    rename PasswordResetTokenRepository

--- src/main/java/com/spring/client/repository/PasswordResetTokenRepository.java
+++ src/main/java/com/spring/client/repository/PasswordResetTokenRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PasswordResetRepository extends JpaRepository<PasswordResetToken, Long> {
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
 
     PasswordResetToken findByToken(String token);
 }
