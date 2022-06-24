3cc362d93caf0842b59fc6843a61865ce22393d9
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-24 09:34:07 +0700

    add method contract service registerUser

--- src/main/java/com/spring/client/service/UserService.java
+++ src/main/java/com/spring/client/service/UserService.java
@@ -1,6 +1,9 @@
 package com.spring.client.service;
 
+import com.spring.client.dto.UserDTO;
+import com.spring.client.entity.User;
+
 public interface UserService {
 
-
+    User registerUser(UserDTO userDTO);
 }
