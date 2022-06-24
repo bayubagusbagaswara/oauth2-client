34eac70b9a97ae6ee95473b20008adf8b04d0987
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-24 10:03:04 +0700

    add method contract service validateVerificationToken and generateNewVerificationToken

--- src/main/java/com/spring/client/service/VerificationTokenService.java
+++ src/main/java/com/spring/client/service/VerificationTokenService.java
@@ -1,8 +1,13 @@
 package com.spring.client.service;
 
+import com.spring.client.entity.User;
 import com.spring.client.entity.VerificationToken;
 
 public interface VerificationTokenService {
+
+    void saveVerificationTokenForUser(String token, User user);
+
+    String validateVerificationToken(String token);
 
     VerificationToken generateNewVerificationToken(String oldToken);
 }
