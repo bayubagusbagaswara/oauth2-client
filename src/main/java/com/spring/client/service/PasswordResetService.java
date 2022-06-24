3c9c50c413e14df1817cf5ad84191579d61c3340
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-24 09:55:04 +0700

    add method contract service createPasswordResetTokenForUser

--- src/main/java/com/spring/client/service/PasswordResetService.java
+++ src/main/java/com/spring/client/service/PasswordResetService.java
@@ -1,4 +1,8 @@
 package com.spring.client.service;
 
+import com.spring.client.entity.User;
+
 public interface PasswordResetService {
+
+    void createPasswordResetTokenForUser(User user, String token);
 }
