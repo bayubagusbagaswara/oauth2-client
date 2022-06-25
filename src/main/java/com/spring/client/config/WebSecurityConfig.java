bb6ce0c16a56dc7eac432a215b00b969de4252dc
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-25 14:19:50 +0700

    add comments

--- src/main/java/com/spring/client/config/WebSecurityConfig.java
+++ src/main/java/com/spring/client/config/WebSecurityConfig.java
@@ -8,6 +8,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+/**
+ * class ini digunakan konfigurasi security untuk resource website
+ */
 @EnableWebSecurity
 public class WebSecurityConfig {
 
